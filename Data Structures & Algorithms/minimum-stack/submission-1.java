7d3888b5cb16d4412be1b5b0e0cb78d67e094cbe
Trieu Thanh Tung <126808438+tungtrieu0801@users.noreply.github.com>
2026-09-14 11:33:34 +0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,33 @@
+class MinStack {
+
+    private Deque<Integer> stack;
+
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || minStack.peek() > val) {
+            minStack.push(val);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
