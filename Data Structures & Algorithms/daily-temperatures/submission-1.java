598aa74ff2baed54d0ecd7168b37a2ecfa31557e
Trieu Thanh Tung <126808438+tungtrieu0801@users.noreply.github.com>
2026-09-15 11:10:23 +0700

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        List<Integer> listResult = new ArrayList<>();
+        Deque<Integer> stackResult = new ArrayDeque<>();
+
+        for (int i = 0; i < temperatures.length; i++) {
+            int day =1;
+            if (i == temperatures.length -1) {
+                listResult.add(0);
+            }
+            for (int j = i + 1; j < temperatures.length; j++) {
+                if (temperatures[i] >= temperatures[j]) {
+                    day += 1;
+                }
+                if(temperatures[i] < temperatures[j]){
+                    listResult.add(day);
+                    break;
+                }
+                if (j == temperatures.length -1) {
+                    listResult.add(0);
+                }
+            }
+
+        }
+
+
+        return listResult.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
